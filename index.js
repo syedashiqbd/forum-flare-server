3d18a4901da2d13280aa1dbf312fe7fb285f31a8
Syed Ashiq <imsyedashiq@gmail.com>
2023-11-26 16:53:49 +0600

    stripe payment intent api added

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-
 const app = express();
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const port = process.env.PORT || 5000;
 
 //Middleware
@@ -134,6 +134,21 @@ async function run() {
       );
 
       res.send(updatedPost);
+    });
+
+    // payment intent
+    app.post('/create-payment-intent', async (req, res) => {
+      const { price } = req.body;
+      const amount = parseInt(price * 100);
+
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: 'usd',
+        payment_method_types: ['card'],
+      });
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
     });
 
     // Send a ping to confirm a successful connection
